f95999d4e2fdd712ce09b1fc2544e55f5df6c274
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-01-21 11:11:52 +0800

    added run_angle function

--- src/maths.rs
+++ src/maths.rs
@@ -2,8 +2,8 @@ use std::f32::consts::PI;
 
 /// a 2-dimensional vector.
 pub struct Vector2{
-    x: f32,
-    y: f32,
+    pub x: f32,
+    pub y: f32,
 }
 
 /// calculates rotation matrix.
@@ -16,7 +16,7 @@ pub struct Vector2{
 /// * angle: the angle in which the point rotates. (degrees)
 /// 
 /// ### Return type: Vector2
-fn find_rotated_point(x1: f32, y1: f32, angle: f32) -> Vector2 {
+pub fn find_rotated_point(x1: f32, y1: f32, angle: f32) -> Vector2 {
     let x2: f32 = (x1 * ((angle*PI/180.0).cos())) + (y1 * (-(angle*PI/180.0).sin()));
     let y2: f32 = (x1 * ((angle*PI/180.0).sin()))+ (y1 * (angle*PI/180.0).cos());
     Vector2 {x: x2, y: y2}
