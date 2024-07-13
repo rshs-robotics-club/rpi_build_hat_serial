b525d823f4c9a3983708ba80873e17adedaa4580
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-07-13 15:13:59 +0800

    added run_pid and run_pwm. run_pwm is the same as the run funciton before.

--- src/motor_wrap.rs
+++ src/motor_wrap.rs
@@ -34,12 +34,36 @@ impl Motor {
             direction: motor_direction,
         }
     }
+    /// Rotates the motor at a given power
+    ///
+    /// # Parameters
+    /// * speed: the power (-100 to 100).
+    pub async fn run_pwm(&mut self, mut speed: f32) {
+        if (speed > 100.0) {
+            speed = 100.0;
+        }
+        else if (speed < -100.0) {
+            speed = -100.0;
+        }
+        // only change the motor if the new speed is different to the previous speed
+        if (speed != self.speed) {
+            let mut serial = UART_SERIAL.lock().await;
+            let _ = send_port(&mut serial, self.port.clone()).await;
+            let _ = send_pwm(&mut serial).await;
+            if (self.direction == Direction::Clockwise) {
+                let _ = send_set_point(&mut serial, speed / 100.0).await;
+            } else {
+                let _ = send_set_point(&mut serial, (speed / 100.0) * -1.0).await;
+            }
+            self.speed = speed;
+        }
+    }
 
     /// Rotates the motor at a given power
     ///
     /// # Parameters
     /// * speed: the power (-100 to 100).
-    pub async fn run(&mut self, mut speed: f32) {
+    pub async fn run_pid(&mut self, mut speed: f32) {
         // only change the motor if the new speed is different to the previous speed
         if (speed > 100.0) {
             speed = 100.0;
@@ -50,12 +74,11 @@ impl Motor {
         
         if (speed != self.speed) {
             let mut serial = UART_SERIAL.lock().await;
-            let pid = format!("pid {} 0 5 s2 0.0027777778 1 0 2.5 0 .4 0.01 \r", port as u8);
-            let _ = send_pwm(&mut serial).await;
+            let pid = format!("pid {} 0 5 s2 0.0027777778 1 0 2.5 0 .4 0.01 \r", self.port.clone() as u8);
             let _ = send_port(&mut serial, self.port.clone()).await;
             let _ = select_mode(&mut serial, 0).await;
             // selrate?
-            let _ = write(&mut serial, pid).await;
+            let _ = write(&mut serial, pid.as_bytes()).await.unwrap();
             if (self.direction == Direction::Clockwise) {
                 let _ = send_set_point(&mut serial, speed / 100.0).await;
             } else {
