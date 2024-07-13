716fd8bd5c188f1a968dda961835f89d044a30d7
Yuet Shing Feng <fengyuetshing@gmail.com>
2024-07-13 13:45:36 +0800

    actually made basic_sensor public

--- src/basic_sensor.rs
+++ src/basic_sensor.rs
@@ -2,14 +2,14 @@ use std::string;
 
 use crate::{raw::{firmware::{self, select_mode, send_plimit, send_port, send_set_point, Port}, read_line}, UART_SERIAL};
 
-struct BasicSensor{
+pub struct BasicSensor{
     port: Port,
 }
 impl BasicSensor{
-    async unsafe fn new(sensor_port: Port) -> Self{
+    pub async unsafe fn new(sensor_port: Port) -> Self{
         Self {port: sensor_port}
     }
-    async unsafe fn read(&self, mode: u8) -> String{
+    pub async unsafe fn read(&self, mode: u8) -> String{
         let mut serial = UART_SERIAL.lock().await;
         let _ = send_port(&mut serial, self.port.clone()).await;
         let _ = send_plimit(&mut serial, 1.0).await;
